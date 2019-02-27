d215c02b1ebf7ed2ff8b62c17c9cf8458fcbf125
Tailiang33yn <48038960+Tailiang33yn@users.noreply.github.com>
2019-02-27 10:27:39 +0800

    Add files via upload

---
+++ stopOracle  (1).bat
@@ -0,0 +1,27 @@
+@echo off
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+if '%errorlevel%' NEQ '0' (
+goto UACPrompt
+) else ( goto gotAdmin )
+:UACPrompt
+echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+"%temp%\getadmin.vbs"
+exit /B
+:gotAdmin
+if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+pushd "%CD%"
+CD /D "%~dp0"
+@echo off
+
+echo 确定要停止Oracle XE服务吗？
+
+pause
+
+net stop "OracleServiceXE"
+
+net stop "OracleXETNSListener"
+
+echo 停止Oracle XE服务完成，请确认有没有错误发生。
+
+Pause
